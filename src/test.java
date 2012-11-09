ffcf275896462afea050c2c2ac937a77db17887a
thinking-aloud <lennart.k@thinking-aloud.eu>
2012-11-09 23:13:07 +0100

    First commit

---
+++ src/test.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: lennart
+ * Date: 11/9/12
+ * Time: 11:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
